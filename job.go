fef72549d3a4ef9ad12e1ebc4d54b9b43fbcf5a5
Lucas Oceano Martins <lucoceano@gmail.com>
2019-06-28 20:37:16 -0300

    Adds job dependency check

--- job.go
+++ job.go
@@ -8,18 +8,37 @@ import (
 var CantDependOnThemselves = fmt.Errorf("jobs can’t depend on themselves")
 
 func OrderJobs(jobs string, dependencies string) (string, error) {
-	if dependencies == "" {
+	if strings.Trim(dependencies, " ") == "" {
 		return jobs, nil
 	}
 
 	var jobsArray []string = strings.Split(jobs, "")
 	var dependenciesArray []string = strings.Split(dependencies, "")
+	var orderedJobs string = ""
 
 	for index, job := range jobsArray {
 		if dependenciesArray[index] == job {
 			return "", CantDependOnThemselves
 		}
+
+		if strings.Index(orderedJobs, job) >= 0 {
+			continue
+		}
+
+		if strings.Index(dependencies, job) == -1 {
+			orderedJobs = appendDependencies(index, jobs, job+orderedJobs, dependenciesArray)
+		}
 	}
 
-	return jobs, nil
+	return orderedJobs, nil
+}
+
+func appendDependencies(index int, jobs string, orderedJobs string, dependenciesArray []string) string {
+	dependency := dependenciesArray[index]
+	if dependency != " " {
+		orderedJobs = dependency + orderedJobs
+		orderedJobs = appendDependencies(strings.Index(jobs, dependency), jobs, orderedJobs, dependenciesArray)
+	}
+
+	return orderedJobs
 }
--- job_test.go
+++ job_test.go
@@ -1,6 +1,9 @@
 package jobs
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestNoJobs(t *testing.T) {
 	expected := ""
@@ -39,5 +42,43 @@ func TestJobsCantDependOnThemselves(t *testing.T) {
 
 	if err != CantDependOnThemselves {
 		t.Errorf("expected '%v' got '%v'", CantDependOnThemselves, err)
+	}
+}
+
+func TestJobsBDependsOnC(t *testing.T) {
+	orderedJobs, _ := OrderJobs("abc", " c ")
+
+	bIndex := strings.Index(orderedJobs, "b")
+	cIndex := strings.Index(orderedJobs, "c")
+
+	if bIndex <= cIndex {
+		t.Errorf("expected c index < b index c = '%d' b = '%d'", cIndex, bIndex)
+	}
+}
+
+func TestJobsBDependsOnCAndCDependsOnFAndDDependsOnAAndEDependsOnB(t *testing.T) {
+	orderedJobs, _ := OrderJobs("abcdef", " cfab ")
+
+	aIndex := strings.Index(orderedJobs, "a")
+	cIndex := strings.Index(orderedJobs, "c")
+	bIndex := strings.Index(orderedJobs, "b")
+	eIndex := strings.Index(orderedJobs, "e")
+	fIndex := strings.Index(orderedJobs, "f")
+	dIndex := strings.Index(orderedJobs, "d")
+
+	if cIndex <= fIndex {
+		t.Errorf("expected f(%d) < c(%d)", fIndex, cIndex)
+	}
+
+	if bIndex <= cIndex {
+		t.Errorf("expected c(%d) < b(%d)", cIndex, bIndex)
+	}
+
+	if eIndex <= bIndex {
+		t.Errorf("expected b(%d) < e(%d)", bIndex, eIndex)
+	}
+
+	if dIndex <= aIndex {
+		t.Errorf("expected a(%d) < d(%d)", aIndex, dIndex)
 	}
 }
